924f0971ef92c2a5a0b3275af1445230de4d6a2f
lenon3579 <lenon3579@gmail.com>
2020-02-11 22:44:20 +0000

    Set Up Project

---
+++ Assets/Scripts/Managers/MapManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager 
+{
+    public static Tile[,] map; // the 2-dimensional map with the information for all the tiles
+}
+
+public class Tile { //Holds all the information for each tile on the map
+    public int xPosition; // the position on the x axis
+    public int yPosition; // the position on the y axis
+    public GameObject baseObject; // the map game object attached to that position: a floor, a wall, etc.
+}
